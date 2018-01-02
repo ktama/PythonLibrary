cdd53be2eec988443fc5d6889405dcb3cf03bbcf
ktama <ktama@users.noreply.github.com>
2018-01-02 13:24:25 +0900

    ランダム回転、ランダム切り抜きを追加

--- DataAugmentation/DataAugmentation.py
+++ DataAugmentation/DataAugmentation.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import skimage
 from skimage import io
 from skimage import transform
+from scipy.misc import imresize
+from scipy.ndimage.interpolation import rotate
 
 class DataAugmentation:
     '''画像のデータ拡張するためのクラス（bmpのみ対応）
@@ -27,10 +29,33 @@ class DataAugmentation:
         skimage.io.imsave(file_name,flip_image)
         return flip_image
 
+    def horizontal_flip(self,file_name='horizontal_flip_image.bmp'):
+        '''水平方向に反転
+        '''
+        flip_image = self.raw_image[:,::-1,:]
+        skimage.io.imsave(file_name,flip_image)
+        return flip_image
+
+    def vertical_flip(self,file_name='vertical_flip_image.bmp'):
+        '''垂直方向に反転
+        '''
+        flip_image = self.raw_image[::-1,:,:]
+        skimage.io.imsave(file_name,flip_image)
+        return flip_image
+
+
     def rotate(self,angle=90,is_resize=False,file_name='rotate_image.bmp'):
         '''指定した角度[0, 360]回転する（is_resize: True→画像が切れないように画像を拡大、False→画像が切れる）
         '''
         rotate_image = skimage.transform.rotate(self.raw_image,angle=angle,resize=is_resize)
+        skimage.io.imsave(file_name,rotate_image)
+        return rotate_image
+
+    def random_rotation(self,angle_range=(0,360),file_name='random_rotate.bmp'):
+        h, w, _ = self.raw_image.shape
+        angle = np.random.randint(*angle_range)
+        rotate_image = rotate(self.raw_image, angle)
+        rotate_image = imresize(rotate_image, (h,w))
         skimage.io.imsave(file_name,rotate_image)
         return rotate_image
 
@@ -53,18 +78,71 @@ class DataAugmentation:
         skimage.io.imsave(file_name,expand_image)
         return expand_image                                                   
 
+    def bright(self,brightness=50,file_name='bright_image.bmp'):
+        '''輝度を比率で変更する（例：1.2は2倍） 調査中
+        '''
+        tmp = self.raw_image
+        bright_image = np.minimum(self.raw_image+brightness,255)
+        # cv2.imwrite(file_name,bright_image)
+        skimage.io.imsave(file_name,bright_image)
+        return bright_image
+
+    def random_crop(self,crop_size=(200,200),file_name='random_crop_image.bmp'):
+        h, w, _ = self.raw_image.shape
+
+        # 0～（画像サイズ - 切り抜きサイズ）の間でtop、leftを決める
+        top = np.random.randint(0, h - crop_size[0])
+        left = np.random.randint(0, w - crop_size[1])
+
+        # 切り抜きサイズからbottomとrightを
+        bottom = top + crop_size[0]
+        right = left + crop_size[1]
+
+        crop_image = self.raw_image[top:bottom, left:right, :]
+        skimage.io.imsave(file_name,crop_image)
+        return crop_image
+
+    def scale_random_crop(self,scale_range=(350,600), crop_size=(340,340),file_name='scale_random_crop_image.bmp'):
+        scale_size = np.random.randint(*scale_range)
+        crop_image = imresize(self.raw_image, (scale_size, scale_size))
+        # crop_image = imresize(self.raw_image, scale_range)
+        h, w, _ = crop_image.shape
+        top = np.random.randint(0, h - crop_size[0])
+        left = np.random.randint(0, w - crop_size[1])
+        bottom = top + crop_size[0]
+        right = left + crop_size[1]
+
+        crop_image = crop_image[top:bottom, left:right, :]
+        skimage.io.imsave(file_name,crop_image)
+        return crop_image
+        
 
 if __name__ == '__main__':
     aug = DataAugmentation()
-    aug.resize((100,100))
-    aug.flip(1, "flip_h_image.bmp")
-    aug.flip(0, "flip_v_image.bmp")
-    for i in range(1, 8):
-        angle = i * 45
-        false_file_name = "rotate"+str(angle)+"F_image.bmp"
-        true_file_name = "rotate"+str(angle)+"T_image.bmp"
-        aug.rotate(angle, False, false_file_name)
-        aug.rotate(angle, True, true_file_name)
+    # aug.resize((100,100))
+    # aug.flip(1, "flip_h_image.bmp")
+    # aug.flip(0, "flip_v_image.bmp")
+    # for i in range(1, 8):
+    #     angle = i * 45
+    #     false_file_name = "rotate"+str(angle)+"F_image.bmp"
+    #     true_file_name = "rotate"+str(angle)+"T_image.bmp"
+    #     aug.rotate(angle, False, false_file_name)
+    #     aug.rotate(angle, True, true_file_name)
 
-    aug.move()
-    aug.expand()
+    # aug.move()
+    # aug.expand()
+    # あとで修正
+    # aug.bright(0.8,'bright1_image.bmp')
+    # aug.bright(1.2,'bright2_image.bmp')
+
+    # aug.horizontal_flip()
+    # aug.vertical_flip()
+    for i in range(10):
+        # crop_name = 'random_crop' + str(i) + '_image.bmp'
+        # aug.random_crop(file_name=crop_name)
+        # scale_crop_name = 'scale_random_crop' + str(i) + '_image.bmp'
+        # aug.scale_random_crop(file_name=scale_crop_name)
+        rotation_random_name = 'rotation_random_crop' + str(i) + '_image.bmp'
+        aug.random_rotation(file_name=rotation_random_name)
+
+
--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Pythonのライブラリ
 [IMACEL ACADEMY || 現役JDと学ぶ画像処理入門①〜openCV入門〜](https://lp-tech.net/articles/qEftT)
 
 [IMACEL ACADEMY || Pythonで画像処理② Data Augmentation (画像の水増し)](https://lp-tech.net/articles/nCvfb)
+
+[kumilog.train || NumPyでの画像のData Augmentationまとめ](http://xkumiyu.hatenablog.com/entry/numpy-data-augmentation)
+
+
+[Qiita || ](https://qiita.com/bohemian916/items/9630661cd5292240f8c7)
