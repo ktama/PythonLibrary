2a9bb63e997bf289b8f9f0a204bb39a173074125
ktama <ktama@users.noreply.github.com>
2018-01-07 02:56:17 +0900

    コメントを追加した。

--- DataAugmentation/DataAugmentation.py
+++ DataAugmentation/DataAugmentation.py
@@ -11,47 +11,41 @@ class DataAugmentation:
     '''画像のデータ拡張するためのクラス（bmpのみ対応）
     '''
     def __init__(self, original_file='Test.bmp'):
-        '''インスタンス作成時に引数で画像ファイルのパスを指定する
-        '''
+        '''インスタンス作成時に引数で画像ファイルのパスを指定する'''
         self.raw_image = skimage.io.imread(original_file)
 
     def resize(self,input_image,size=(16,16), file_name='resize_image.bmp'):
-        '''引数で指定したサイズへ変更する
-        '''
+        '''引数で指定したサイズへ変更する'''
         resize_image = skimage.transform.resize(input_image,size)
         skimage.io.imsave(file_name,resize_image)
         return resize_image
 
     def flip(self,input_image,is_horizontal=1,file_name='flip_image.bmp'):
-        '''反転する（is_horizontal: True→水平、False→垂直）
-        '''
+        '''反転する（is_horizontal: True→水平、False→垂直）'''
         flip_image = cv2.flip(input_image,is_horizontal)
         skimage.io.imsave(file_name,flip_image)
         return flip_image
 
     def horizontal_flip(self,input_image,file_name='horizontal_flip_image.bmp'):
-        '''水平方向に反転
-        '''
+        '''水平方向に反転'''
         flip_image = input_image[:,::-1,:]
         skimage.io.imsave(file_name,flip_image)
         return flip_image
 
     def vertical_flip(self,input_image,file_name='vertical_flip_image.bmp'):
-        '''垂直方向に反転
-        '''
+        '''垂直方向に反転'''
         flip_image = input_image[::-1,:,:]
         skimage.io.imsave(file_name,flip_image)
         return flip_image
 
-
     def rotate(self,input_image,angle=90,is_resize=False,file_name='rotate_image.bmp'):
-        '''指定した角度[0, 360]回転する（is_resize: True→画像が切れないように画像を拡大、False→画像が切れる）
-        '''
+        '''指定した角度[0, 360]回転する（is_resize: True→画像が切れないように画像を拡大、False→画像が切れる）'''
         rotate_image = skimage.transform.rotate(input_image,angle=angle,resize=is_resize)
         skimage.io.imsave(file_name,rotate_image)
         return rotate_image
 
     def random_rotation(self,input_image,angle_range=(0,360),file_name='random_rotate.bmp'):
+        '''ランダムに角度を回転させる'''
         h, w, _ = input_image.shape
         angle = np.random.randint(*angle_range)
         rotate_image = rotate(input_image, angle)
@@ -60,8 +54,7 @@ class DataAugmentation:
         return rotate_image
 
     def move(self,input_image,x=100,y=100,file_name='move_image.bmp'):
-        '''x,y方向へ平行移動する
-        '''
+        '''x,y方向へ平行移動する'''
         height, width = input_image.shape[:2]
         # 変換行列の作成
         M = np.float32([[1,0,x],[0,1,y]])
@@ -70,8 +63,7 @@ class DataAugmentation:
         return move_image
 
     def expand(self,input_image,rate=0.5,file_name='expand_image.bmp'):
-        '''比率を指定して拡大する（例：0.5は50%拡大）
-        '''
+        '''比率を指定して拡大する（例：0.5は50%拡大）'''
         size = input_image.shape[0]
         matrix = skimage.transform.AffineTransform(scale=(1-rate,1-rate),translation=(size*rate/2,size*rate/2))
         expand_image = skimage.transform.warp(input_image,matrix)
@@ -79,14 +71,14 @@ class DataAugmentation:
         return expand_image                                                   
 
     def bright(self,input_image,brightness=50,file_name='bright_image.bmp'):
-        '''輝度を比率で変更する（例：1.2は2倍） 調査中
-        '''
+        '''輝度を比率で変更する（例：1.2は2倍） 調査中'''
         bright_image = np.minimum(input_image+brightness,255)
         # cv2.imwrite(file_name,bright_image)
         skimage.io.imsave(file_name,bright_image)
         return bright_image
 
     def random_crop(self,input_image,crop_size=(200,200),file_name='random_crop_image.bmp'):
+        '''サイズを指定してランダムに画像を切り出す'''
         h, w, _ = input_image.shape
 
         # 0～（画像サイズ - 切り抜きサイズ）の間でtop、leftを決める
@@ -102,6 +94,7 @@ class DataAugmentation:
         return crop_image
 
     def scale_random_crop(self,input_image,scale_range=(350,600), crop_size=(340,340),file_name='scale_random_crop_image.bmp'):
+        '''scale_rangeで指定したサイズの範囲で拡大してから、crop_sizeのサイズでランダムに画像を切り出す'''
         scale_size = np.random.randint(*scale_range)
         crop_image = imresize(input_image, (scale_size, scale_size))
         crop_image = self.random_crop(crop_image, crop_size)
@@ -109,6 +102,7 @@ class DataAugmentation:
         return crop_image
 
     def cutout(self,input_image,mask_size=100,file_name='cutout_image.bmp'):
+        '''ランダムに指定したサイズをマスクする'''
         cutout_image = np.copy(input_image)
         mask_value =cutout_image.mean()
 
@@ -129,6 +123,7 @@ class DataAugmentation:
         return cutout_image
 
     def erasing(self,input_image,mask_size_range=(0.02, 0.4), aspect_range=(0.3, 3),file_name='erasing_image.bmp'):
+        '''ランダムに指定したサイズを消去する'''
         erasing_image = np.copy(input_image)
         mask_value = np.random.randint(0, 256)
 
@@ -151,6 +146,35 @@ class DataAugmentation:
         skimage.io.imsave(file_name,erasing_image)
         return erasing_image
 
+    def gauss_noise(self,input_image,sigma=15,file_name='gauss_image.bmp'):
+        '''ガウスノイズを付与する 調査中'''
+        gauss_image = np.copy(input_image)
+        row, col, ch = gauss_image.shape
+        mean = 0
+        gauss = np.random.normal(mean, sigma, (row, col, ch))
+        gauss = gauss.reshape(row, col, ch)
+        gauss_image = gauss_image + gauss
+        skimage.io.imsave(file_name,gauss_image)
+        return gauss_image
+
+    def salt_noise(self,input_image,rate=4,file_name='salt_image.bmp'):
+        '''ソルトノイズを付与する'''
+        salt_image = np.copy(input_image)
+        num_salt = np.ceil((rate/100) * salt_image.size)
+        coords = [np.random.randint(0, i-1, int(num_salt)) for i in salt_image.shape]
+        salt_image[coords[:-1]] = (255,255,255)
+        skimage.io.imsave(file_name,salt_image)
+        return salt_image
+        
+    def pepper_noise(self,input_image,rate=4,file_name='pepper_image.bmp'):
+        '''ペッパーノイズを付与する'''
+        pepper_image = np.copy(input_image)
+        num_pepper = np.ceil((rate/100) * pepper_image.size)
+        coords = [np.random.randint(0, i-1, int(num_pepper)) for i in pepper_image.shape]
+        pepper_image[coords[:-1]] = (0,0,0)
+        skimage.io.imsave(file_name,pepper_image)
+        return pepper_image
+
 if __name__ == '__main__':
     aug = DataAugmentation()
     # aug.resize(input_image=aug.raw_image,size=(100,100))
@@ -169,7 +193,7 @@ if __name__ == '__main__':
 
     # aug.horizontal_flip(input_image=aug.raw_image)
     # aug.vertical_flip(input_image=aug.raw_image)
-    # for i in range(10):
+    for i in range(10):
         # crop_name = 'random_crop' + str(i) + '_image.bmp'
         # aug.random_crop(input_image=aug.raw_image,file_name=crop_name)
         # scale_crop_name = 'scale_random_crop' + str(i) + '_image.bmp'
@@ -180,5 +204,12 @@ if __name__ == '__main__':
         # aug.cutout(input_image=aug.raw_image,file_name=cutout_name)
         # erase_name = 'erase' + str(i) + '_image.bmp'
         # aug.erasing(input_image=aug.raw_image,file_name=erase_name)
+        # あとで修正
+        # gauss_name = 'gauss' + str(i) + '_image.bmp'
+        # aug.gauss_noise(input_image=aug.raw_image,file_name=gauss_name)
+        # salt_name = 'salt' + str(i) + '_image.bmp'
+        # aug.salt_noise(input_image=aug.raw_image,file_name=salt_name)
+        # pepper_name = 'pepper' + str(i) + '_image.bmp'
+        # aug.pepper_noise(input_image=aug.raw_image,file_name=pepper_name)
 
 
